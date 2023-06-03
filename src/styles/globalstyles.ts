a087a46a9f4eeb6f979108ce469ef7367d93108f
Barba2k2 <lorenzo.dz@hotmail.com>
2023-06-03 15:40:56 -0300

    Inicio do Projeto

---
+++ src/styles/globalstyles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+html{
+    @media(max-width: 1440px){
+        font-size: 93.75%;
+    }
+    @media(max-width: 375px){
+        // font-size: 87.5%
+    }
+}
+
+body {
+    -webkit-font-smoothing: antialiased;
+}
+
+body {
+    font-family: "Heebo", sans-serif;
+}
+
+:root {
+    --space-light: #3D68D2;
+    --space-dark: #040327;
+    --space: #0B1E8A;
+    --gray-01: #0D0E13;
+    --gray-02: #2C2D3A;
+    --gray-03: #60616F;
+    --gray-04: #898A93;
+    --gray-05: #D5D5DB;
+    --mars: #E85937
+    --mars-light: #FF8C70;
+    --mars-dark: #CF3F1D;
+    --sun: #F5D15F;
+    --jupiter: #E00E34;
+    --earth: #0FDD86;
+    --saturn: #554AD7;
+    --uranus: #24FFE9;
+}
+`
